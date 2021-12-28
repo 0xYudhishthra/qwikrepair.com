554dfb16623d3178739a157cde2b84e3a180bad7
Bryan Choong <bryanchoong12@gmail.com>
2021-12-28 18:12:31 +0800

    init

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Qwikrepair</title>
+    <link rel="icon" type="image/x-icon" href="src/logo.svg">
+    <link type="stylesheet" href=src/main.css">
+    <link type="script" defer href="js/main.js">
+</head>
+<body>
+</body>
+</html>
