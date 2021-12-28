acae8c5fc4289057cb1e023241f077002b7abee8
Bryan Choong <bryanchoong12@gmail.com>
2021-12-29 06:25:49 +0800

    tune

--- js/navbar.js
+++ js/navbar.js
@@ -4,6 +4,5 @@ var path = arrayPath[arrayPath.length - 1].slice(0,-4);
 
 element = document.getElementById(path);
 element.removeAttribute("href");
-element.style.cursor = "pointer";
 element.style.fontWeight = "bold";
 element.style.borderBottom = "1.5px solid black"
