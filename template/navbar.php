701c8b6449a4685efdae84e8ef23ef65bf6d54b6
Bryan Choong <bryanchoong12@gmail.com>
2021-12-28 19:48:05 +0800

    file structure

---
+++ template/navbar.php
@@ -0,0 +1,15 @@
+<div class="navbar">
+    <a class="logo" href="">
+        <div class="logo-svg" src="src/logo.svg"></div>
+        <div class="logo-word">Qwikrepair</div>
+    </a>
+    <div class="btn-navigation">
+        <a class="about-us" href="aboutus.php">About Us</a>
+        <a class="faq" href="faq.php">FAQ</a>
+        <a class="services" href="services.php">Services</a>
+    </div>
+    <div class="btn-access">
+        <a class="btn btn1 login" href="login.php">Login</a>
+        <a class="btn btn2 register" href="register.php">Register</a>
+    </div>
+</div>
