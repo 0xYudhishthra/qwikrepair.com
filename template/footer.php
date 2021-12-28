28b05f7f35de9ce09603657467a7f3f63f26664c
Bryan Choong <bryanchoong12@gmail.com>
2021-12-29 04:17:53 +0800

    footer

---
+++ template/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <a class="logo" href="">
+        <img class="logo-svg" src="src/logo.svg">
+        <div class="logo-word">Qwikrepair</div>
+    </a>
+    <div class="email font-small">support@qwikrepair.com</div>
+    <div class="link-wrapper">
+        <a class="link" href="">Home</a>
+        <a class="link" href="aboutus.php">About Us</a>
+        <a class="link" href="services.php">Services</a>
+        <a class="link" href="faq.php">FAQ</a>
+    </div>
+    <div class="copyright">Copyright &copy; 2022 Qwikrepair Sdn Bhd. All Rights Reserved.</div>
+</footer>
