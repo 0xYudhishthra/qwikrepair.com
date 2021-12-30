fb9f30025f421320374778ccbf0e122b13e12a43
Yudhish Maran <66850474+yudhx@users.noreply.github.com>
2021-12-30 19:13:58 +0800

    Add connection verification

--- api/dbConnection.php
+++ api/dbConnection.php
@@ -9,4 +9,11 @@ $password = "";
 //Establish connection to database
 $conn = new mysqli($host, $user, $password, $database);
 
-?>
+if ($conn->connect_errno) {
+    http_response_code(400);
+    header("Content-Type: text/plain");
+  echo $conn -> connect_error;
+  exit();
+}
+
+?> 
