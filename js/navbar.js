047fe60014fc85ead3d8fafc9761e5a3b87688d0
Bryan Choong <bryanchoong12@gmail.com>
2021-12-30 18:33:21 +0800

    navbar fix

--- js/navbar.js
+++ js/navbar.js
@@ -5,4 +5,5 @@ var path = arrayPath[arrayPath.length - 1].slice(0,-4);
 element = document.getElementById(path);
 element.removeAttribute("href");
 element.style.fontWeight = "bold";
-element.style.borderBottom = "1.5px solid black"
+element.style.color = "#94D1EA";
+element.style.borderBottom = "1.5px solid #94D1EA"
