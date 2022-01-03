befa0a8371b6015c2d6b7474d517b26109625c88
Bryan Choong <bryanchoong12@gmail.com>
2022-01-03 19:50:03 +0800

    signup css

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,32 @@
+passwordHide = document.getElementById('password-hide');
+passwordInput = document.getElementById('password');
+rptPasswordHide = document.getElementById('rptPassword-hide');
+rptPasswordInput = document.getElementById('rptPassword');
+
+if ((passwordHide != null) && (passwordInput != null)) {
+    passwordHide.addEventListener('click', () => {
+        if (passwordInput.getAttribute('type') == 'password') {
+            passwordInput.setAttribute('type', 'text');
+            passwordHide.setAttribute('src', 'src/eye.svg');
+        } else {
+            passwordInput.setAttribute('type', 'password');
+            passwordHide.setAttribute('src', 'src/eye-crossed.svg');
+        } // else
+    }) //eventListener
+} //if != null
+
+if ((rptPasswordHide != null) && (rptPasswordInput != null)) {
+    rptPasswordHide.addEventListener('click', () => {
+        if (rptPasswordInput.getAttribute('type') == 'password') {
+            rptPasswordInput.setAttribute('type', 'text');
+            rptPasswordHide.setAttribute('src', 'src/eye.svg');
+        } else {
+            rptPasswordInput.setAttribute('type', 'password');
+            rptPasswordHide.setAttribute('src', 'src/eye-crossed.svg');
+        } // else
+    }) //eventListener
+} //if != null
+
 const signup = document.querySelector('input[type="submit"]');
 var httpStatus
 signup.addEventListener('click', () => {
--- signup.php
+++ signup.php
@@ -1,46 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Signup</title>
-        <!-- <?php include "template/header.php" ?> -->
-    <script src="js/signup.js" defer></script>
-    <style>
-    h1{
-        text-align: center;
-        margin-top: 50px;
-    }
-    div{
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 10px;
-    }
-    form{
-        width: 60vw;
-        margin: auto;
-    }
-    </style>
+    <?php include "template/header.php" ?>
+        <title>Qwikrepair - Signup</title>
+        <link type="text/css" rel="stylesheet" href="css/navbar.css">
+        <link type="text/css" rel="stylesheet" href="css/footer.css">
+        <link type="text/css" rel="stylesheet" href="css/signup.css">
+        <script src="js/signup.js" defer></script>
     </head>
     <body>
-        <h1>Signup</h1>
-        <form onsubmit="return false;">
-            <div><label>First Name</label><input type="text" name="firstName"></div>
-            <div><label>Last Name</label><input type="text" name="lastName"></div>
-            <div><label>Phone Number</label><input type="tel" pattern="[0-9]{10}" name="pNumber" placeholder="E.g. 0112334567"></div>
-            <div><label>Date of Birth</label><input type="date" name="dob"></div>
-            <div><label>Street</label><input type="text" name="street"></div>
-            <div><label>City</label><input type="text" name="city"></div>
-            <div><label>State</label><input type="text" name="state"></div>
-            <div><label>Postcode</label><input type="text" name="postcode" pattern="[0-9]{5}" placeholder="E.g. 45678"></div>
-            <div><label>Email</label><input type="email" name="email"></div>
-            <div><label for="">Password</label><input type="password" name="pwd"></div>
-            <div><label for="">Repeat Password</label><input type="password" name="repeatPwd"></div>
-            <div><label for="">Role</label>
+        <?php include "template/navbar.php" ?>
+        <div class="content-wrapper">
+            <div class="ask-login-wrapper">
+                <a class="font font-text ask-login" href="login.php">Already own an account?</a>
+            </div>
+            <div class="font font-title title">Signup</div>
+            <form class="form" onsubmit="return false;">
+                <!-- <label>First Name</label>
+                <input type="text" name="firstName" placeholder="First Name">
+                <label>Last Name</label>
+                <input type="text" name="lastName" placeholder="Last Name">
+                <label>Phone Number</label>
+                <input type="tel" pattern="[0-9]{10}" name="pNumber" placeholder="E.g. 0112334567">
+                <label>Date of Birth</label>
+                <input type="date" name="dob" placeholder="Date Of Birth">
+                <label>Street</label>
+                <input type="text" name="street" placeholder="Street">
+                <label>City</label>
+                <input type="text" name="city" placeholder="City">
+                <label>State</label>
+                <input type="text" name="state" placeholder="State">
+                <label>Postcode</label>
+                <input type="text" name="postcode" pattern="[0-9]{5}" placeholder="E.g. 45678"> -->
+                <label class="font font-medium label">Email</label>
+                <input class="input" type="email" name="email" placeholder="Email">
+                <label class="font font-medium label">Password</label>
+                <div>
+                    <input id="password" class="input password-input" type="password" name="pwd" placeholder="Password">
+                    <img id="password-hide" class="password-hide" src="src/eye-crossed.svg">
+                </div>
+                <label class="font font-medium label">Repeat Password</label>
+                <div>
+                    <input id="rptPassword" class="input password-input" type="password" name="repeatPwd" placeholder="Password">
+                    <img id="rptPassword-hide" class="password-hide" src="src/eye-crossed.svg">
+                </div>
+                <label class="font font-medium label">Role</label>
                 <select name="role">
                     <option value="senior">Senior</option>
                     <option value="technician">Technician</option>
                 </select>
-            </div>
-            <div><input type="hidden" name="request-type" value="signup"></div>
-            <div><input type="submit" value="Signup"></div>
-        </form>
+                <div><input type="hidden" name="request-type" value="signup"></div>
+                <div><input class="btn btn-blue signup-btn" type="submit" value="Signup"></div>
+            </form>
+        </div>
+        
+        <?php include "template/footer.php" ?>
     </body>
