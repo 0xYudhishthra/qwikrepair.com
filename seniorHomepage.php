ad1fbd8b899b0fe49eba6902dea9911dbbe3b09a
Yudhish Maran <66850474+yudhx@users.noreply.github.com>
2022-01-19 21:33:52 +0800

    .

--- seniorHomepage.php
+++ seniorHomepage.php
@@ -6,21 +6,6 @@
     <link type="text/css" rel="stylesheet" href="css/navbar.css">
     <link type="text/css" rel="stylesheet" href="css/navbarUser.css">
     <link type="text/css" rel="stylesheet" href="css/sideNav.css">
-<<<<<<< HEAD
-</head>
-<body>
-    <?php include "template/navbarUser.php" ?>
-    <?php include "template/sideNav.php" ?>
-    <div class="content-wrapper">
-        <div class="greet-wrapper">
-            <div class="greet-hi">Hello, world!</div>
-            <div class="greet-hope">hope you are having a good day!</div>
-            <div class="greet-date">Hayday, 1 Cucumber</div>
-        </div>
-        <div class="appo-history">
-            <div class="appo-title">Past Appointments</div>
-            
-=======
     <link type="text/css" rel="stylesheet" href="css/seniorHomepage.css">
     <script type="text/javascript" src="js/seniorHomepage.js" defer></script>
 </head>
@@ -67,7 +52,6 @@
                     </div>
                 </div>
             </div>
->>>>>>> parent of 1308de2 (Restructure)
         </div>
     </div>
 </body>
