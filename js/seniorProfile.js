c955781ff584b8c40c71a40243fd4b5babc590b6
Yudhish Maran <66850474+yudhx@users.noreply.github.com>
2022-01-21 01:24:15 +0800

    Flexible element type specification

--- js/seniorProfile.js
+++ js/seniorProfile.js
@@ -2,12 +2,14 @@ Element.prototype.appendAfter = function (element) {
     element.parentNode.insertBefore(this, element.nextSibling);
 }, false;
 
-function instantiateElement(data, elementID){
-    var element = document.createElement('div');
-    element.innerHTML = data;
-    element.id = elementID;
-    return element;
-}
+function instantiateElement(data, elementID, elementType) {
+        var element = document.createElement(elementType);
+        element.innerHTML = data;
+        element.id = elementID;
+        element.appendChild(document.createElement("br"));
+        return element;
+};
+
 document.addEventListener("DOMContentLoaded", function() {
     const requestType = new FormData(document.querySelector('form'));
     fetch('api/crudHandler.php', {
@@ -18,15 +20,15 @@ document.addEventListener("DOMContentLoaded", function() {
         return res.json();
     })
     .then(data => {
-        instantiateElement(data['firstName'] + ' ' + data['lastName'], 'fullNameData').appendAfter(document.getElementById('fullName'));
-        instantiateElement(data['email'], 'emailData').appendAfter(document.getElementById('emailAddress'));
-        instantiateElement(data['role'], 'roleData').appendAfter(document.getElementById('role'));
-        instantiateElement(data['dob'], 'dobData').appendAfter(document.getElementById('dob'));
-        instantiateElement(data['phoneNumber'], 'phoneNumberData').appendAfter(document.getElementById('phoneNumber'));
-        instantiateElement(data['street'], 'streetData').appendAfter(document.getElementById('street'));
-        instantiateElement(data['city'], 'cityData').appendAfter(document.getElementById('city'));
-        instantiateElement(data['state'], 'stateData').appendAfter(document.getElementById('state'));
-        instantiateElement(data['postcode'], 'postcodeData').appendAfter(document.getElementById('postcode'));
+        instantiateElement(data['firstName'] + ' ' + data['lastName'], 'fullNameData', 'div').appendAfter(document.getElementById('fullName'));
+        instantiateElement(data['email'], 'emailData', 'p').appendAfter(document.getElementById('emailAddress'));
+        instantiateElement(data['role'], 'roleData', 'p').appendAfter(document.getElementById('role'));
+        instantiateElement(data['dob'], 'dobData', 'h3').appendAfter(document.getElementById('dob'));
+        instantiateElement(data['phoneNumber'], 'phoneNumberData', 'h3').appendAfter(document.getElementById('phoneNumber'));
+        instantiateElement(data['street'], 'streetData', 'h3').appendAfter(document.getElementById('street'));
+        instantiateElement(data['city'], 'cityData', 'h3').appendAfter(document.getElementById('city'));
+        instantiateElement(data['state'], 'stateData', 'h3').appendAfter(document.getElementById('state'));
+        instantiateElement(data['postcode'], 'postcodeData', 'h3').appendAfter(document.getElementById('postcode'));
     })
     .catch(err => {
         console.log(err);
