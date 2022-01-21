ce64aa389bb90cfa30eac0e2d7dab230e70aa1e4
Yudhish Maran <66850474+yudhx@users.noreply.github.com>
2022-01-21 22:20:24 +0800

    Add reference to ionicons

--- template/header.php
+++ template/header.php
@@ -3,6 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
 <link rel="icon" type="image/x-icon" href="src/logo.svg">
 <link type="text/css" rel="stylesheet" href="css/general.css">
 <script src="js/general.js" defer></script>
