7c23704441eff7361824effa5d8300a74a184d2d
Yudhish Maran <66850474+yudhx@users.noreply.github.com>
2022-01-21 22:20:09 +0800

    Add sample FAQs

--- faq.php
+++ faq.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>FAQ</title>
     <?php include "template/header.php" ?>
     <link type="text/css" rel="stylesheet" href="css/navbar.css">
     <link type="text/css" rel="stylesheet" href="css/footer.css">
@@ -8,11 +9,52 @@
 </head>
 <body>
     <?php include "template/navbar.php" ?>
-    <div class="font-title">SERVICES</div>
-    <div class="font-large">font-large</div>
-    <div class="font-medium">font-medium</div>
-    <div class="font-text">font-text (default, no declare required)</div>
-    <div class="font-small">font-small</div>
+    <section>
+        <div class="container">
+            <div class="faq">
+                <div class="faqQuestion" id="question1">
+                    <a class="faqLink" href="#question1">
+                        How can I book an appointment?
+                        <i class="icon ion-md-add"></i>
+                        <i class="icon ion-md-remove"></i>
+                    </a>
+                    <div class="faqAnswer">
+                        <p>lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="faqQuestion" id="question2">
+                    <a class="faqLink" href="#question2">
+                        How can I cancel an appointment?
+                        <i class="icon ion-md-add"></i>
+                        <i class="icon ion-md-remove"></i>
+                    </a>
+                    <div class="faqAnswer">
+                        <p>lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="faqQuestion" id="question3">
+                    <a class="faqLink" href="#question3">
+                        How can I change an appointment?
+                        <i class="icon ion-md-add"></i>
+                        <i class="icon ion-md-remove"></i>
+                    </a>
+                    <div class="faqAnswer">
+                        <p>lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div class="faqQuestion" id="question4">
+                    <a class="faqLink" href="#question4">
+                        How can I view my appointment history?
+                        <i class="icon ion-md-add"></i>
+                        <i class="icon ion-md-remove"></i>
+                    </a>
+                    <div class="faqAnswer">
+                        <p>lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <?php include "template/footer.php" ?>
 </body>
 </html>
