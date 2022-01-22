a916172c3e467432a5f7deacb0e79806e675dcf8
Yudhish Maran <66850474+yudhx@users.noreply.github.com>
2022-01-23 07:57:33 +0800

    Initial technician pages

---
+++ technicianHistory.php
@@ -0,0 +1,40 @@
+<?php session_start(); 
+    if (isset($_SESSION['email']) && $_SESSION['role'] !== 'senior')
+        header("Location: index.php");
+    if (!isset($_SESSION['email']))
+        header("Location: index.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>History</title>
+    <?php include "template/header.php" ?>
+    <link type="text/css" rel="stylesheet" href="css/navbar.css">
+    <link type="text/css" rel="stylesheet" href="css/navbarUser.css">
+    <link type="text/css" rel="stylesheet" href="css/sideNav.css">
+    <link type="text/css" rel="stylesheet" href="css/seniorHistory.css">
+    <script src="js/seniorHistory.js" defer></script>
+</head>
+<body>
+    <?php include "template/navbarUser.php" ?>
+    <div>
+        <?php include "template/sideNav.php" ?>
+        <div class="content-wrapper">
+        <div class="font font-large title">Appointment History</div>
+            <div class="table-wrapper">
+                <table class="table" id="appointmentHistoryTable">
+                    <tr class="tableHeaders">
+                        <th class="date-h">Date</th>
+                        <th class="time-h">Time</th>
+                        <th class="service-name-h">Service Name</th>
+                        <th class="tech-name-h">Technician</th>
+                        <th class="review-h">Review</th>
+                    </tr>
+                </table>
+                <!-- <div class="btn btn-blue load-more">Load More</div> -->
+            </div>
+        </div>
+    </div>
+</body>
+</html>
