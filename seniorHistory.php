ba539758cc9dacd7bfb91b87eb49f20fd3427c1f
Bryan Choong <bryanchoong12@gmail.com>
2022-01-22 08:42:23 +0800

    senior history

--- seniorHistory.php
+++ seniorHistory.php
@@ -19,24 +19,98 @@
     <?php include "template/navbarUser.php" ?>
     <div>
         <?php include "template/sideNav.php" ?>
-        <h1>Appointment History</h1><br>
-        <table class="historyTable">
-            <tr>
-                <th>Company</th>
-                <th>Contact</th>
-                <th>Country</th>
-            </tr>
-            <tr>
-                <td>Alfreds Futterkiste</td>
-                <td>Maria Anders</td>
-                <td>Germany</td>
-            </tr>
-            <tr>
-                <td>Centro comercial Moctezuma</td>
-                <td>Francisco Chang</td>
-                <td>Mexico</td>``
-            </tr>
-        </table>
+        <div class="content-wrapper">
+        <div class="font font-large title">Appointment History</div>
+            <div class="table-wrapper">
+                <table class="table">
+                    <tr>
+                        <th class="date-t">Date</th>
+                        <th class="time-h">Time</th>
+                        <th class="service-name-h">Service Name</th>
+                        <th class="tech-name-h">Technician</th>
+                        <th class="review-h">Review</th>
+                    </tr>
+                    <tr>
+                        <td class="date-t">31/12/2021</td>
+                        <td class="time-h">12.00 pm</td>
+                        <td class="service-name-h">Plumbing</td>
+                        <td class="tech-name-h">Mr. Elon</td>
+                        <td class="review-h">stars</td>
+                    </tr>
+                    <tr>
+                        <td class="date-t">31/12/2021</td>
+                        <td class="time-h">12.00 pm</td>
+                        <td class="service-name-h">Plumbing</td>
+                        <td class="tech-name-h">Mr. Elon</td>
+                        <td class="review-h">stars</td>
+                    </tr>
+                    <tr>
+                        <td class="date-t">31/12/2021</td>
+                        <td class="time-h">12.00 pm</td>
+                        <td class="service-name-h">Plumbing</td>
+                        <td class="tech-name-h">Mr. Elon</td>
+                        <td class="review-h">stars</td>
+                    </tr>
+                    <tr>
+                        <td class="date-t">31/12/2021</td>
+                        <td class="time-h">12.00 pm</td>
+                        <td class="service-name-h">Plumbing</td>
+                        <td class="tech-name-h">Mr. Elon</td>
+                        <td class="review-h">stars</td>
+                    </tr>
+                    <tr>
+                        <td class="date-t">31/12/2021</td>
+                        <td class="time-h">12.00 pm</td>
+                        <td class="service-name-h">Plumbing</td>
+                        <td class="tech-name-h">Mr. Elon</td>
+                        <td class="review-h">stars</td>
+                    </tr>
+                    <!-- <tr>
+                        <td class="date-t">31/12/2021</td>
+                        <td class="time-h">12.00 pm</td>
+                        <td class="service-name-h">Plumbing</td>
+                        <td class="tech-name-h">Mr. Elon</td>
+                        <td class="review-h">stars</td>
+                    </tr>
+                    <tr>
+                        <td class="date-t">31/12/2021</td>
+                        <td class="time-h">12.00 pm</td>
+                        <td class="service-name-h">Plumbing</td>
+                        <td class="tech-name-h">Mr. Elon</td>
+                        <td class="review-h">stars</td>
+                    </tr>
+                    <tr>
+                        <td class="date-t">31/12/2021</td>
+                        <td class="time-h">12.00 pm</td>
+                        <td class="service-name-h">Plumbing</td>
+                        <td class="tech-name-h">Mr. Elon</td>
+                        <td class="review-h">stars</td>
+                    </tr>
+                    <tr>
+                        <td class="date-t">31/12/2021</td>
+                        <td class="time-h">12.00 pm</td>
+                        <td class="service-name-h">Plumbing</td>
+                        <td class="tech-name-h">Mr. Elon</td>
+                        <td class="review-h">stars</td>
+                    </tr>
+                    <tr>
+                        <td class="date-t">31/12/2021</td>
+                        <td class="time-h">12.00 pm</td>
+                        <td class="service-name-h">Plumbing</td>
+                        <td class="tech-name-h">Mr. Elon</td>
+                        <td class="review-h">stars</td>
+                    </tr>
+                    <tr>
+                        <td class="date-t">31/12/2021</td>
+                        <td class="time-h">12.00 pm</td>
+                        <td class="service-name-h">Plumbing</td>
+                        <td class="tech-name-h">Mr. Elon</td>
+                        <td class="review-h">stars</td>
+                    </tr> -->
+                </table>
+                <div class="btn btn-blue load-more">Load More</div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
